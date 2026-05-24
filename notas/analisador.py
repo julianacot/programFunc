175fca2cef981be12b60c72e75584c3e1e15aefa
julianacot <132670022+julianacot@users.noreply.github.com>
2026-05-24 10:37:45 +0000

    FALTA E FINAL

--- notas/analisador.py
+++ notas/analisador.py
@@ -7,6 +7,7 @@ alunos = list(map(
     lambda x: {
         'nome': x['nome'],
         'extra': int(x['extra']),
+        'faltas': int(x['faltas']),
         'notas': list(map(float, [x['nota1'], x['nota2'], x['nota3'], x['nota4']]))
     }, 
     dados))
@@ -18,10 +19,39 @@ MediaAlunos = list(map(lambda x: {
     'media': min(sum(x['notas']) / len(x['notas'])+ 1 if x['extra'] == 1 else sum(x['notas']) / len(x['notas']),10)
 },alunos))
 
-aprovados = list(filter(lambda x: x['media'] >=7, MediaAlunos))
-reprovados = list(filter(lambda x: x['media'] <7, MediaAlunos))
+aprovadosMedia = list(filter(lambda x: x['media'] >=7, MediaAlunos))
+recuperacao = list(filter(lambda x: x['media'] <7, MediaAlunos))
+reprovadoFalta = list(filter(
+    lambda x: x['faltas'] >= 15,
+    alunos
+))
 
-print(aprovados)
-print(reprovados)
-
+print("XXXXXAPROVADO POR MÉDIA: xxxxx")
+print(aprovadosMedia)
+print("xxxxx REPROVADO POR FALTAS:xxxxx")
+print(reprovadoFalta)
+print("xxxxx RECUPERAÇÃO:xxxxx")
+print(recuperacao)
+print("xxxxx   MÉDIA DE ALUNOS: xxxxx")
 print(MediaAlunos)
+
+with open('final.csv', 'r') as arquivo_final:
+    leitor_final = csv.DictReader(arquivo_final, delimiter=';')
+    dados_final = list(leitor_final)
+
+alunosFinal = list(map(
+    lambda x: {
+        'nome': x['nome'],
+        'notaFinal': float(x['nota5'])
+    }, 
+    dados_final))
+
+resultadoFinal = list(map(
+    lambda x: {
+        'nome': x['nome'],
+        'status': 'AprovadoFinal' if x['notaFinal'] >= 5 else 'Reprovado'
+    },
+    alunosFinal
+))
+
+print(resultadoFinal)
